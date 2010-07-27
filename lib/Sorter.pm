fecce052a7e9c5c5aace4be0593a4647ea57a5db
shiba_yu36 <shiba1029196473@gmail.com>
2010-07-28 00:14:18 +0900

    getとset

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,23 @@
-# implement this
+package Sorter;
+use strict;
+use warnings;
+
+
+
+sub new {
+    my $class = shift;
+    bless {}, $class;
+}
+
+sub set_values {
+    my ($self, @args) = @_;
+    $self->{values} = \@args;
+}
+
+sub get_values {
+    my $self = shift;
+    return if !$self->{values};
+    return @{ $self->{values} };
+}
+
+1;
