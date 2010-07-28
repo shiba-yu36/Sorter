f3e5c9cad8bf33a0e925ee6ee25e97b82541f69b
shiba_yu36 <shiba1029196473@gmail.com>
2010-07-28 13:25:20 +0900

    とりあえずテストは通ったので、完成。もう少し速くしたい。

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -12,12 +12,38 @@ sub new {
 sub set_values {
     my ($self, @args) = @_;
     $self->{values} = \@args;
+    return @{ $self->{values} };
 }
 
 sub get_values {
     my $self = shift;
     return if !$self->{values};
     return @{ $self->{values} };
+}
+
+sub push {
+    my ($self, $value) = @_;
+    $self->set_values($self->get_values, $value);
+    return $self;
+}
+
+sub sort {
+    my $self = shift;
+    my @values = $self->get_values;
+
+    return @values if @values <= 1;
+
+    my $left  = __PACKAGE__->new;
+    my $right = __PACKAGE__->new;
+
+    my $pivot = shift @values;              # とりあえず最初を取る感じで
+
+    for my $value (@values) {
+        $left->push($value)  if $value <= $pivot;
+        $right->push($value) if $value > $pivot;
+    }
+
+    $self->set_values($left->sort, $pivot, $right->sort);
 }
 
 1;
